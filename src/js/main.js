f6cc635b0e5a993db77eb9e401c6b77b78690002
christiano0407 <placebo0704@gmail.com>
2024-04-05 13:02:58 -0600

    add new design and layout mobile

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@
 const navBar = document.querySelector("#navMobile"); 
 const displayMobile = document.querySelector("#displayMobile");
 const navMenu = document.querySelector("#idNavigation"); 
+const menuListIcon = document.querySelectorAll(".menu__list--nav");
