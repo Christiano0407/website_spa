b5503c2768ed8da74f8916499ec133a406a90329
christiano0407 <placebo0704@gmail.com>
2024-04-08 11:54:12 -0600

    Add: Add new fetch of data

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,42 @@
-//** === Variables === */
+//import { readFile } from "fs/promises"; //Get Of System File 
 const navMobile = document.querySelector("#navMobile"); 
 const displayMobile = document.querySelector("#displayMobile");
 const navMenu = document.querySelector("#idNavigation"); 
 const btnMenu = document.querySelectorAll(".menu__list--nav");
 
 
+
+const dataTrip = async () => {
+  try {
+    //const data = await readFile(new URL('../data/data.json', import.meta.url));
+    const response = await fetch('../data/data.json');
+    if(response.status === 200) {
+      const data = await response.json()
+      console.log(data);
+
+      const showData = (d) => {
+        for (const item of d) {
+          console.log(item); 
+        }
+      }
+
+      showData(data);
+      
+    }else {
+      throw new Error(`Error to Get Data, ${response.status}`); 
+    }
+   
+  }catch(err) {
+    console.error("Error to Read Data Json", err);
+  }
+}
+
+dataTrip();
+
+
 document.addEventListener('DOMContentLoaded', () => { 
    btnMenu.forEach((btn) => {
-    btn.addEventListener("click", (e)=> {
+    btn.addEventListener("click", (e) => {
       e.preventDefault();
       console.log("Click Menu");
      });
