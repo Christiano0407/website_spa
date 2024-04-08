78979ea359a1e4d0f1496364610a96aaa88295fa
christiano0407 <placebo0704@gmail.com>
2024-04-08 08:49:42 -0600

    Add: New Search and Events call btns Events. Btn Events Call Display Mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 //** === Variables === */
-const navBar = document.querySelector("#navMobile"); 
+const navMobile = document.querySelector("#navMobile"); 
 const displayMobile = document.querySelector("#displayMobile");
 const navMenu = document.querySelector("#idNavigation"); 
-const menuListIcon = document.querySelectorAll(".menu__list--nav");
+const btnMenu = document.querySelectorAll(".menu__list--nav");
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+   btnMenu.forEach((btn) => {
+    btn.addEventListener("click", (e)=> {
+      e.preventDefault();
+      console.log("Click Menu");
+     });
+   });
+}); 
