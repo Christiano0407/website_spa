f31a4dea7a062256cdd623fb52ab091a89f89595
christiano0407 <placebo0704@gmail.com>
2024-04-14 15:59:32 -0600

    add: All Information to Travel

--- src/poo/patternsDesign.js
+++ src/poo/patternsDesign.js
@@ -42,11 +42,11 @@ export class CityDestination extends Destination {
   getDestinyDetail() {
     return `
       <div class="destination__data">
-        <h2 class="destination__data--title >${this.destinationData.Destination}</h2>
-        <p class="destination__data--traveler >Traveler: ${this.destinationData.TravelerName}</p>
-        <p class="destination__data--start >Start Date: ${this.destinationData.StartDate}</p>
-        <p class="destination__data--end >End Date: ${this.destinationData.EndDate}</p>
-        <p class="destination__data--time >Duration: ${this.destinationData.Duration} days</p>
+        <h2 class="destination__data--title"> Destiny: ${this.destinationData.Destination}</h2>
+        <p class="destination__data--traveler"> Traveler: ${this.destinationData.TravelerName}</p>
+        <p class="destination__data--start"> Start Date: ${this.destinationData.StartDate}</p>
+        <p class="destination__data--end"> End Date: ${this.destinationData.EndDate}</p>
+        <p class="destination__data--time"> Duration: ${this.destinationData.Duration} days</p>
       </div>
     `;  
   } 
@@ -69,9 +69,9 @@ export class TravelLodging extends Travel {
   getLodgingDetails() {
     return `
       <div class="travel__destiny">
-        <h2 travel__destiny--destiny >Destination:${this.dataTravel.Destination}</h2>
-        <p travel__destiny--type >AccommodationType:${this.dataTravel.AccommodationType}</p>
-        <p travel__destiny--cost >AccommodationCost:${this.dataTravel.AccommodationCost}</p>
+        <h2 class="travel__destiny--destiny"> Destination:${this.dataTravel.Destination}</h2>
+        <p class="travel__destiny--type"> AccommodationType:${this.dataTravel.AccommodationType}</p>
+        <p "travel__destiny--cost"> AccommodationCost:${this.dataTravel.AccommodationCost}</p>
       </div>
     `; 
   }
@@ -94,9 +94,9 @@ export class TravelTransport extends Travel {
   getTransportDetails() {
     return ` 
       <div class="travel__transport">
-        <h2 travel__transport--destiny >Destination: ${this.dataTravel.Destination}</h2>
-        <p travel__transport--type >TransportationType: ${this.dataTravel.TransportationType}</p>
-        <p travel__transport--cost >TransportationCost: ${this.dataTravel.TransportationCost}</p>
+        <h2 class="travel__transport--destiny"> Destination: ${this.dataTravel.Destination}</h2>
+        <p class="travel__transport--type"> TransportationType: ${this.dataTravel.TransportationType}</p>
+        <p class="travel__transport--cost"> TransportationCost: ${this.dataTravel.TransportationCost}</p>
       </div>
     `; 
   }
