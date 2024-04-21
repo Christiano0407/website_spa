a2b898337c94c907d3fb6876d4025f8e9c4d6206
christiano0407 <placebo0704@gmail.com>
2024-04-21 12:54:24 -0600

    add proves api

--- src/js/pages/travels.js
+++ src/js/pages/travels.js
@@ -1,1 +1,9 @@
 //** === Used Google Maps (Static Maps - ApiKey) === */
+{/* <img 
+src="https://maps.googleapis.com/maps/api/staticmap?
+center=51.5072,-0.1275&
+zoom=14&
+size=400x400&
+maptype=roadmap&
+markers=color:red|label:C|51.5072,-0.1275&
+key=> */}
